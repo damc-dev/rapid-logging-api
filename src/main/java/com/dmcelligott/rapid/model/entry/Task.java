6221896c2f5609ffff05939ac63d928389e61820
David McElligott <damc.dev@gmail.com>
2015-08-20 15:31:49 -0500

    added getters/setters

--- src/main/java/com/dmcelligott/rapid/model/entry/Task.java
+++ src/main/java/com/dmcelligott/rapid/model/entry/Task.java
@@ -7,7 +7,7 @@ import org.pojomatic.annotations.AutoProperty;
 public class Task {
 	private String title;
 	private String description;
-	private boolean isCompleted;
+	private boolean completed;
 
 	public Task() {
 	}
@@ -21,10 +21,34 @@ public class Task {
 		this.description = description;
 	}
 	
-	public Task(String title, String description, boolean isCompleted) {
+	public Task(String title, String description, boolean completed) {
 		this.title = title;
 		this.description = description;
-		this.isCompleted = isCompleted;
+		this.completed = completed;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean getCompleted() {
+		return completed;
+	}
+
+	public void setCompleted(boolean completed) {
+		this.completed = completed;
 	}
 
 	@Override
@@ -40,13 +64,5 @@ public class Task {
 	@Override
 	public String toString() {
 		return Pojomatic.toString(this);
-	}
-
-	public boolean isCompleted() {
-		return isCompleted;
-	}
-
-	public void setCompleted(boolean isCompleted) {
-		this.isCompleted = isCompleted;
 	}
 }
